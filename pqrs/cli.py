b06932b3119e622db9782751ae263c25c26cc77f
Tomas Babej <tomas@tbabej.com>
2021-04-09 23:46:09 -0400

    subscribe: Implement rudimentary version of the command

--- pqrs/cli.py
+++ pqrs/cli.py
@@ -1,11 +1,18 @@
 import typer
 import plumbum
+from datafiles import datafile
 from plumbum.cmd import git
 from plumbum import local, FG, BG
 
-app = typer.Typer(add_completion=False)
 
 PQRS_LOCATION = local.env.home / '.pqrss'
+
+app = typer.Typer(add_completion=False)
+
+@datafile(str(PQRS_LOCATION / "pqrs.yml"))
+class Config:
+    channels: dict[str, str] = None
+    roles: dict[str, list[str]] = None
 
 
 @app.command()
@@ -26,8 +33,13 @@ def subscribe(url: str):
     Subscribe to a given channel.
     """
 
-    pass
+    galaxy = local["ansible-galaxy"]
+    result = galaxy["collection", "install", url].run()
+    namespace, collection = result[1].splitlines()[-1].split()[0].split('.')
 
+    config = Config.objects.get_or_create()
+    config.channels[f"{namespace}.{collection}"] = url
+    config.roles[f"{namespace}.{collection}"] = None
 
 def run():
     app()
