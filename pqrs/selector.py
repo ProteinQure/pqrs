afeafc17236c3c2fa49a4e35e7a10bcc232a77df
Tomas Babej <tomas@tbabej.com>
2021-04-11 22:04:24 -0400

    selector: Add basic application

---
+++ pqrs/selector.py
@@ -0,0 +1,95 @@
+import npyscreen
+
+
+class DescribedCheckBox(npyscreen.CheckBox):
+    """
+    Checkbox that displays a description of the given checkbox in the parent form.
+    Note: This expects that the parent form has a widget attribute called 'description'.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.parent_form = kwargs.pop('parent_form')
+        self.description_text = kwargs.pop('description_text')
+        super().__init__(*args, **kwargs)
+
+    def on_focusin(self):
+        if not getattr(self.parent_form, 'description', None):
+            return None
+        self.parent_form.description.values = self.description_text
+        self.parent_form.display()
+
+    def on_focusout(self):
+        if not getattr(self.parent_form, 'description', None):
+            return None
+        self.parent_form.description.values = []
+
+
+class RoleSelectorForm(npyscreen.Form):
+    """
+    Form displaying the available roles and their description.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.data = kwargs.pop('data')
+        self.checkboxes = []
+        super().__init__(*args, **kwargs)
+
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
+
+    def create(self):
+        for role, description in self.data.items():
+            checkbox = self.add(
+                DescribedCheckBox,
+                name=role,
+                max_width=40,
+                parent_form=self,
+                description_text=description
+            )
+            self.checkboxes.append(checkbox)
+
+        self.description = self.add(
+            npyscreen.BoxTitle,
+            name="Description",
+            relx=50,
+            rely=1,
+            editable=False,
+            max_height=20
+        )
+
+
+class RoleSelector(npyscreen.NPSAppManaged):
+    """
+    Ncurses-based application to select appropriate roles to install on the
+    workstation.
+    """
+
+    def __init__(self, data):
+        self.data = data
+        super().__init__()
+
+    def onStart(self):
+        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
+        self.addForm('MAIN', RoleSelectorForm, name='Select roles', data=self.data)
+
+
+def run_selector(data):
+    """
+    Returns user-selected roles.
+    """
+    app = RoleSelector(data)
+    app.run()
+
+    return [
+        role for role, checkbox in zip(data, app._Forms["MAIN"].checkboxes)
+        if checkbox.value is True
+    ]
+
+
+if __name__ == '__main__':
+    data = {
+        'basic': ["All the usual stuff"],
+        'cli-vim': ["Vim goodies"],
+        'cli-bash': ["You should probably install this", "also configures history"],
+    }
+    print(run_selector(data))
