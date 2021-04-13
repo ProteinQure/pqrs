c6e368fdcad6c63ec1e511330f1c2225e9dc8771
Tomas Babej <tomas@tbabej.com>
2021-04-13 13:42:50 -0400

    config: Supress messages coming out of datafiles library

--- pqrs/config.py
+++ pqrs/config.py
@@ -1,10 +1,14 @@
 """
 Implements interface for the PQRS configuration file.
 """
+import logging
 
 from datafiles import datafile
 
 from pqrs import paths
+
+# Supress info messages from datafiles
+logging.getLogger('datafiles').setLevel(logging.CRITICAL)
 
 
 @datafile(str(paths.PQRS_LOCATION / "pqrs.yml"))
